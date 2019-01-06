6d2443817c870ae263d95e9d605e8f1cf17348c7
Dmitry Sadovnikov <sadovnikov.js@gmail.com>
2019-01-06 12:27:12 +0300

    init

---
+++ taurus/FirstViewController.swift
@@ -0,0 +1,21 @@
+//
+//  FirstViewController.swift
+//  taurus
+//
+//  Created by Dmitry Sadovnikov on 2019-01-06.
+//  Copyright © 2019 enwords. All rights reserved.
+//
+
+import UIKit
+
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+
+}
---
+++ taurus/SecondViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SecondViewController.swift
+//  taurus
+//
+//  Created by Dmitry Sadovnikov on 2019-01-06.
+//  Copyright © 2019 enwords. All rights reserved.
+//
+
+import UIKit
+
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+
+}
